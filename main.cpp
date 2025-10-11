00352bb7411130a37459f5e04725f9b6708f424b
dmitriimotuzov <dimamotuzov07@gmail.com>
2025-10-12 00:23:15 +0300

    write traverse the tree and output all tokens

--- main.cpp
+++ main.cpp
@@ -2,11 +2,40 @@
 #include <slang/syntax/SyntaxPrinter.h>
 #include <iostream>
 #include <prettyprint.h>
+#include "slang/syntax/SyntaxPrinter.h"
+
+#include "slang/parsing/ParserMetadata.h"
+#include "slang/syntax/SyntaxNode.h"
+#include "slang/syntax/SyntaxTree.h"
+#include "slang/text/SourceLocation.h"
+#include "slang/text/SourceManager.h"
 
 using namespace slang;
 using namespace slang::syntax;
 using namespace slang::parsing;
 
+
+void print_childs(const SyntaxNode &root){
+    //std::cout << "root : " << root.kind << "\n";
+    //std::cout << "subtree end \n";
+
+    slang::size_t count_child = root.getChildCount();
+    
+    for(slang::size_t i = 0; i < count_child; i++){
+        if (auto childNode = root.childNode(i); childNode){
+            print_childs(*childNode);
+        }
+            
+        else if (auto token = root.childToken(i); token){
+            std::cout << "token start\n";
+            std::cout << token.toString() << "\n";
+            std::cout << token.kind << "\n";
+            std::cout << token.rawText() << "\n";
+            std::cout << "token end\n";
+        } 
+    }
+    return;
+}
 
 int main(int argc, char **argv){
     slang::driver::Driver driver;
@@ -16,19 +45,16 @@ int main(int argc, char **argv){
     }
     bool ok = driver.parseAllSources();
     if (!ok) {
-        std::cout << "error: failed to parse input files\n";
+        std::cerr << "error: failed to parse input files\n";
         return 1;
     }
 
     if (driver.syntaxTrees.empty()) {
-        std::cout << "error: failed to parse input files\n";
+        std::cerr << "error: failed to parse input files\n";
         return 1;
     }
     auto tree = driver.syntaxTrees.back();
-    SyntaxPrinter printer(driver.sourceManager);
-    printer.setIncludeDirectives(true);
-    printer.setSquashNewlines(false);
-    std::string output = printer.print(*tree).str();
-    printf("%s", output.c_str());
+    print_childs(tree->root());
+
     return 0;
 }
